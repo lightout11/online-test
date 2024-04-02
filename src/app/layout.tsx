088328e582b517efddc4f912196808074c3dce29
lightout11 <lightout01@outlook.com>
2024-04-03 00:28:48 +0700

    change to async

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   description: "Thi Online",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
